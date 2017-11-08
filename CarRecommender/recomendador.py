979752643e40cc6e9bd8bd330cc19e4d69f74df8
Pablo Real Fernández <prealfer@gmail.com>
2017-11-09 00:26:26 +0100

    Subida way!

--- CarRecommender/recomendador.py
+++ CarRecommender/recomendador.py
@@ -1,14 +1,16 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 from scipy.spatial import distance
 import pandas as pd
 
 
 
-# In[18]:
+# ## Método que recomienda 20 vehículos segun contenido sugerido por el usuario, sin filtrar por precios. 
+
+# In[ ]:
 
 normalizada = pd.read_csv('normalizado.csv')
 normalizada.drop('Unnamed: 0',axis=1,inplace=True)
@@ -31,7 +33,27 @@ def recomienda_20(_list):
     return df_listado.ix[df_dist.sort_values('dist', ascending=True).head(20).index]
 
 
-# In[ ]:
+# ## Método que recomienda 20 vehículos segun contenido sugerido por el usuario, filtrando por el presupuesto disponible:
+# 
+# ### Argumentos de entrada: Lista de 17 valores, 16 normalizados y un presupuesto sin normalizar.
 
+# In[13]:
 
+def recomienda_20F(_list):
+
+    recorrido = range(len(normalizada))
+    distancias = []
+    presupuesto_normalizado = (_list[0] - df_listado.precio.min())/(df_listado.precio.max()-df_listado.precio.min())
+    presupuesto_maximo = (_list[0] + (2*_list[0])/10)
+    presupuesto_minimo = (_list[0] - (3*_list[0])/10)
+    _list.remove(_list[0])
+    _list.insert(0,presupuesto_normalizado)
+    
+    for i in recorrido:
+        distancias.append(distance.euclidean(_list, array_norm[i]))
+        
+    df_dist = pd.DataFrame({'dist':distancias, 'ind':recorrido})
+    df_recommend = df_listado.iloc[df_dist.sort_values('dist', ascending=True).index]
+    
+    return df_recommend[(df_recommend['precio']<presupuesto_maximo)&(df_recommend['precio']>presupuesto_minimo)].head(20)
 
--- CarRecommender/run.py
+++ CarRecommender/run.py
@@ -1,7 +1,7 @@
 from flask import *
 import pandas as pd
 from flask import request
-from recomendador import recomienda_20
+from recomendador import recomienda_20F
 
 app = Flask(__name__)
 
@@ -11,20 +11,31 @@ def index():
 
 @app.route("/result",methods = ['POST', 'GET'])
 def result():
-    
+    data_vector = []
     if request.method == 'POST':
+        for value in request.form.values():
+            data_vector.append(float(value))
+        
+        for value,key in request.form.iteritems(): 
+            print(value,key)
+        
+        d0 = data_vector[0]
+        d1 = data_vector[1]
+        
+        data_vector.remove(d0)
+        data_vector.remove(d1)
+        
+        data_vector.insert(0,d0)
+        data_vector.insert(0,d1)
+        
+        print(data_vector)
     
-        #return render_template('result.html',result=request.form)
-    
-    #return render_template('result.html',result=result)
-        data = recomienda_20([1,1,1,0,0,0,1,1,0,1,0,1,0,0,1,1,0])
-        #data = pd.read_csv('listado.csv').head()
-      #data.set_index(['Name'], inplace=True)
-      #data.index.name=None
-      #females = data.loc[data.Gender=='f']
-      #males = data.loc[data.Gender=='m']
-        return render_template('views.html',tables=[data.to_html(classes='female')],titles=['na','coches'])
-        #return render_template('view.html',tables=[females.to_html(classes='female'), males.to_html(classes='male')],titles = ['na', 'Female surfers', 'Male surfers'])
+        data = recomienda_20F(data_vector)
+        #data = recomienda_20([1,1,1,0,0,0,1,1,0,1,0,1,0,0,1,1,0])
 
+        #return render_template('views.html',tables=[data.to_html(classes='female', bold_rows=True)],titles=['Recomendacion','coches'])
+
+    
+        return render_template('views.html',tables=[data.to_html()],titles=['Recomendacion'])
 if __name__ == "__main__":
     app.run(debug=True)
